8a3a0f61c69c4b5029a2974d88f0d00828a79a79
Aman Ahmed <aman.ahmed97@gmail.com>
2023-10-14 21:39:26 -0400

    added ITs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,19 @@ android {
     }
 }
 
+//dependencies {
+//
+//    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.10.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    testImplementation("junit:junit:4.13.2")
+//
+//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+//    implementation("androidx.activity:activity-ktx:1.4.0")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//}
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -42,9 +55,10 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
     implementation("androidx.activity:activity-ktx:1.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
 }
--- app/src/androidTest/java/com/example/motiontrackingapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/motiontrackingapplication/ExampleInstrumentedTest.kt
@@ -1,24 +1,60 @@
 package com.example.motiontrackingapplication
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 
+import androidx.test.uiautomator.UiDevice
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private lateinit var scenario: ActivityScenario<MainActivity>
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.motiontrackingapplication", appContext.packageName)
     }
+
+    @Test
+    fun testSensitivitySeekBar(){
+        scenario = launch(MainActivity::class.java)
+        Espresso.onView(withId(R.id.seekBar)).perform(ViewActions.click())
+        Espresso.onView(withId(R.id.textView))
+            .check(matches(withText("50.0")))
+        Thread.sleep(3000)
+        scenario.close()
+    }
+
+    @Test
+    fun testMotion(){
+        scenario = launch(MainActivity::class.java)
+
+        val device = UiDevice.getInstance(getInstrumentation())
+        device.setOrientationNatural()
+        device.setOrientationRight()
+        device.setOrientationLeft()
+        device.setOrientationNatural()
+
+        scenario.close()
+    }
+
 }
